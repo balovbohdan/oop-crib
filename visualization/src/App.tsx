3f4d51a1a2aefab1fdefcc0ac614f950f5ec2805
balovbohdan <balovbohdan@gmail.com>
2022-03-29 20:11:11 +0300

    basename

--- visualization/src/App.tsx
+++ visualization/src/App.tsx
@@ -9,8 +9,7 @@ import Layout from './Layout';
 import Home from './pages/home';
 import DesignPatterns from './pages/design-patterns';
 
-//{ basename: process.env.PUBLIC_URL }
-const history = createBrowserHistory();
+const history = createBrowserHistory({ basename: process.env.PUBLIC_URL });
 
 const Pages = () => (
   <Layout>
